9c5ccd03079c2bae9b762b7412b6eb38cf8093a4
OnGarces <cgaop7@gmail.com>
2019-06-22 14:44:51 -0500

    updt public dir v3

--- app/server/server.js
+++ app/server/server.js
@@ -6,17 +6,24 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const path = require("path");
+const node_env_var = process.env.NODE_ENV;
 
 class Server {
 
     constructor(puerto) {
         this.port = puerto;
         this.app = express();
+
+
     }
 
     publicFolder() {
+        if (node_env_var === 'production') {
+            var publicPath2 = path.join(process.cwd(), './public');
+        } else {
+            var publicPath2 = path.join(process.cwd(), '../public');
+        }
         //const publicPath = path.resolve(__dirname, '../public');
-        const publicPath2 = path.join(process.cwd(), '../public');
         //this.app.use(express.static(publicPath));
         this.app.use('/static', express.static(publicPath2))
         console.log(`>>public path >>>${publicPath2}\n--http://localhost:8080/static/qr_image.png--`); //>>${publicPath}\n
