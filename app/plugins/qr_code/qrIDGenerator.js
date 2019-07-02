e878161d50133b3e7a3343f44d997b71c7860a36
OnGarces <cgaop7@gmail.com>
2019-07-02 00:25:54 -0500

    bd connection done - queries pending

--- app/plugins/qr_code/qrIDGenerator.js
+++ app/plugins/qr_code/qrIDGenerator.js
@@ -3,16 +3,14 @@ const generateIdQR = (jsonD) => {
     let correo_split = jsonD.user.email.split('@')[0].slice();
     let idQr = correo_split + fechastr;
     return idQr;
+    //emailmmddhhmmss
 }
 
 const generateCleanDate = () => {
     let fechastr = new Date().toISOString().replace('-', '').replace('.', '').replace('T', '').replace('Z', '').replace('-', '').replace(':', '').replace(':', '').slice(4, -3);
     return fechastr;
+    //0629221401 - mmddhhmmss
 }
-
-/**
- * {"user":{"nombre":"cri","apellido":"grccs","cedula":"1111","email":"cristian_garces82121@elpoli.edu.co","celular":"333","tipoPersona":"estudiante","fecha":"2019-06-29","motivoVisita":"xxxxx"}}
- */
 
 module.exports = {
     generateIdQR,
--- app/router/api.js
+++ app/router/api.js
@@ -4,9 +4,11 @@ const express = require('express');
 const router = express.Router();
 const plugins = require('../plugins');
 const dotenv = require("dotenv");
+const { Client } = require('pg');
 dotenv.config();
 
 const MAIL_USER = process.env.MAIL_USER;
+const POSTGRES_URI = process.env.POSTGRES_URI;
 
 router.use(express.urlencoded());
 router.use(express.json());
@@ -18,7 +20,7 @@ router.use(function(req, res, next) {
 });
 
 router.get("/api/hello", (req, res) => {
-    console.log(`-> GET ${req.path} ->COMUNICACION: ${MAIL_USER}`);
+    console.log(`\n-> GET ${req.path}`);
     res.send({ express: `ayuda:-> ${MAIL_USER}` });
 });
 
@@ -32,17 +34,77 @@ router.get("/api/validateqr", (req, res) => {
 router.post('/api/generateqr', (req, res) => {
 
     console.log(`-> POST ${req.path}`);
+    const client = new Client({
+        connectionString: POSTGRES_URI,
+        ssl: true,
+    });
 
-    var reqJsonBody = req.body;
-    var data = JSON.stringify(reqJsonBody);
-    let idQr = plugins.qr.qr_id_generate.generateIdQR(reqJsonBody);
-
+    var reqJson = req.body;
+    var data = JSON.stringify(reqJson);
+    let idQr = plugins.qr.qr_id_generate.generateIdQR(reqJson);
     console.log(`-> The request data is:\n${data}\nid QR:${idQr}`);
-    plugins.qr.qr_generate.generateQR(idQr, reqJsonBody);
-    plugins.mail.mail_send.sendTheMail(idQr, reqJsonBody);
 
-    res.json({
-        ok: true
+    const queryText = 'INSERT INTO public.solicitud_ingreso(id_solicitud, id_usuario, nombre_usuario, email_usuario, tipo_usuario, fecha_visita, motivo_visita) values($1, $2, $3, $4, $5, $6, $7)';
+    const values = [idQr, reqJson.user.cedula, reqJson.user.nombre + ' ' + reqJson.user.apellido, reqJson.user.email, reqJson.user.tipoPersona, reqJson.user.fecha, reqJson.user.motivoVisita];
+
+    client.connect((err, done) => {
+        if (err) {
+            done();
+            console.log(`error conectando db, path: ${req.path} ` + err);
+            return res.status(500).json({ success: false, data: err });
+        }
+        client.query(queryText, values)
+            .then(response => {
+                console.log('response length: ' + response.rows.length);
+                client.end()
+                plugins.qr.qr_generate.generateQR(idQr, reqJson);
+                plugins.mail.mail_send.sendTheMail(idQr, reqJson);
+                return res.status(200).send({
+                    ok: true
+                });
+            })
+            .catch(err => {
+                client.end();
+                console.log('error query insert not excecuted: ' + err);
+                return res.status(500).send('Error, query insert not excecuted' + err);
+            });
+    });
+});
+
+router.get('/api/todos', (req, res, next) => {
+    console.log(`-> GET ${req.path}`);
+    const client = new Client({
+        connectionString: POSTGRES_URI,
+        ssl: true,
+    })
+    const queryText = 'SELECT * FROM solicitud_ingreso';
+
+    client.connect((err, done) => {
+        if (err) {
+            done();
+            console.log(`error conectando db, path: ${req.path} ` + err);
+            return res.status(500).json({ success: false, data: err });
+        }
+        client.query(queryText)
+            .then(response => {
+                if (response.rows.length < 1) {
+                    client.end()
+                    res.status(404).send({
+                        status: 'Failed',
+                        message: 'No requests information found',
+                    });
+                } else {
+                    client.end()
+                    res.status(200).send({
+                        ok: true,
+                        data: response.rows
+                    });
+                }
+            })
+            .catch(err => {
+                client.end()
+                return res.status(500).json({ success: false, data: 'error en query ' + queryText + ' -> ' + err });
+            });
     });
 });
 
